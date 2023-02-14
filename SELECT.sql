2e385fcbe1e3f58c02d1033e372cff92c77950bd
Valentin_Silivankou <litin20011995@gmail.com>
2023-02-14 22:07:54 +0300

    Change SELECT

--- SELECT.sql
+++ SELECT.sql
@@ -1,27 +1,34 @@
-SELECT title, year_of_release 
+--Название и год выхода альбомов, вышедших в 2018 году
+SELECT album_title, year_of_release
   FROM albums_of_executor
  WHERE year_of_release = '2018';
  
+--Название и продолжительность самого длительного трека
 SELECT tracks_title, duration 
   FROM tracks
  ORDER BY duration DESC
  LIMIT 1;
  
+--Название треков, продолжительность которых не менее 3,5 минуты
 SELECT tracks_title, duration 
   FROM tracks
- WHERE duration >= '00:03:30';
+ WHERE duration >= '00:03:30'
+ORDER BY duration DESC;
  
-SELECT title, year_of_release 
+--названия сборников, вышедших в период с 2018 по 2020 год включительно
+SELECT compiliation_title 
   FROM compiliations_of_tracks
  WHERE year_of_release BETWEEN 2018 AND 2020;
  
+--Исполнители, чье имя состоит из 1 слова
 SELECT nickname 
   FROM executors
  WHERE nickname NOT LIKE '% %';
- 
+
+--Название треков, которые содержат слово "мой"/"my".
 SELECT tracks_title 
   FROM tracks
- WHERE tracks_title LIKE '%Мой%'
-    OR tracks_title LIKE '%мой%'
-    OR tracks_title LIKE '%My%'
-    OR tracks_title LIKE '%my%';
+ WHERE tracks_title LIKE '%Мой%' 
+ 	OR tracks_title LIKE '%мой%'
+ 	OR tracks_title LIKE '%My%'
+ 	OR tracks_title LIKE '%my%';
